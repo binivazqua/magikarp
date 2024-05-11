783200936709be914c1d8e582c7149dc66f406bd
binivazqua <binizavazquez@gmail.com>
2024-05-11 16:58:09 -0600

    nav menu basis

---
+++ main.js
@@ -0,0 +1,7 @@
+/* =========== HEADER AND NAV =============== */
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+/* Menu is shown */ 
