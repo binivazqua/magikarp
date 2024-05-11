42da455182951dd4db8aa181e0d9e79122280a1c
binivazqua <binizavazquez@gmail.com>
2024-05-11 17:13:05 -0600

    nav js basic func

--- main.js
+++ main.js
@@ -4,4 +4,28 @@ const navMenu = document.getElementById('nav-menu'),
     navClose = document.getElementById('nav-close')
 
 
-/* Menu is shown */ 
+/* Menu is shown */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* ====== menu on mobile ======= */
+/* quitar el menu cuando le das click */
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    /* cuando click en cada elemento, quitar la class de show menu*/
+    navMenu.classList.remove('show-menu')
+
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
