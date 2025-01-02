5f8f7cc41ca9c5dd6cc39cf684b2783305769c4b
binivazqua <binizavazquez@gmail.com>
2025-01-02 15:41:34 -0600

    done with auto slider without transitions

--- awards.js
+++ awards.js
@@ -16,6 +16,18 @@ next.onclick = function () {
     reloadSlider();
 }
 
+prev.onclick = function () {
+    if (active - 1 < 0) {
+        active = lengthItems;
+    } else {
+        active = active - 1;
+    }
+
+    reloadSlider();
+}
+
+let refreshSlider = setInterval(() => { next.click() }, 3000);
+
 function reloadSlider() {
     let checkLeft = items[active].offsetLeft;
     slider_list.style.left = -checkLeft + 'px';
@@ -23,4 +35,14 @@ function reloadSlider() {
     let lastActiveDot = document.querySelector('.slider .dots li.active');
     lastActiveDot.classList.remove('active');
     dots[active].classList.add('active');
+
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(() => { next.click() }, 3000);
 }
+
+dots.forEach((list, key) => {
+    list.addEventListener('click', function () {
+        active = key;
+        reloadSlider();
+    })
+})
