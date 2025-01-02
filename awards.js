588d76c25c9428ccbf7872262f7064c5bba8e8bc
binivazqua <binizavazquez@gmail.com>
2025-01-02 12:14:11 -0600

    added js file for slider

--- awards.js
+++ awards.js
@@ -3,3 +3,6 @@ let items = document.querySelectorAll('.slider .list .item');
 let dots = document.querySelectorAll('.slider .dots li');
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
+
+let active = 0;
+
